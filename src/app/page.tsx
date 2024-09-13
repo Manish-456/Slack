f7a7c819b38abc80c7bcbcf772e527dab1ced4e2
Manish Tamang <111108242+Manish-456@users.noreply.github.com>
2024-09-13 14:13:21 +0545

    Fix loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
-import { UserButton } from "@/features/auth/components/user-button";
+import { Loader } from "lucide-react";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModalAtom } from "@/features/workspaces/store/use-create-workspace-modal";
 
@@ -24,8 +24,8 @@ export default function Home() {
   }, [workspaceId, isLoading, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground"/>
     </div>
   );
 }
